8495ccff06502bb69e6abb08a874b4fb93f2679e
RicardoLuis0 <ricolvs123@gmail.com>
2019-04-15 22:26:44 -0300

    Corrigir Erro #2

--- inc/require_user.php
+++ inc/require_user.php
@@ -1,7 +1,7 @@
 <?php
-	include("require_logged");
+	include("require_logged.php");
 	if($_SESSION["is_admin"]){
-		$_SESSION['403_message']="Funcionalidade não disponível para administradores";
+		$_SESSION['403_message']="Funcionalidade nÃ£o disponÃ­vel para administradores";
 		header("location:access_denied.php");
 		exit();
 	}
