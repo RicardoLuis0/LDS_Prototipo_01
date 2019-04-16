18d5cb1c453cf8bd42c2157b05c70da337c14bda
RicardoLuis0 <ricolvs123@gmail.com>
2019-04-15 22:26:29 -0300

    Corrigir erro

--- inc/require_admin.php
+++ inc/require_admin.php
@@ -1,7 +1,7 @@
 <?php
-	include("require_logged");
+	include("require_logged.php");
 	if(!$_SESSION["is_admin"]){
-		$_SESSION['403_message']="Nível de Acesso Insuficiente";
+		$_SESSION['403_message']="NÃ­vel de Acesso Insuficiente";
 		header("location:access_denied.php");
 		exit();
 	}
