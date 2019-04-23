8d55cce0fbc10abf4fd4249edd1b4515ed784d62
Ricardo <ricolvs123@gmail.com>
2019-04-23 18:39:30 -0300

    move code to function

--- access_denied.php
+++ access_denied.php
@@ -1,22 +1,25 @@
 <?php
-include("inc/session_setup.php");
+require_once("inc/session_setup.php");
 $id=-1;
 $title_name="Acesso Negado";
-if(isset($_SESSION["403_message"])){
-	if(isset($_SESSION["403_message_absolute"])&&$_SESSION["403_message_absolute"]){
-		$message="<p>".$_SESSION["403_message"].".</p>";
-		unset($_SESSION["403_message_absolute"]);
+function get_message():string{
+	if(isset($_SESSION["403_message"])){
+		if(isset($_SESSION["403_message_absolute"])&&$_SESSION["403_message_absolute"]){
+			$message="<p>".$_SESSION["403_message"].".</p>";
+			unset($_SESSION["403_message_absolute"]);
+		}else{
+			$message='<p>Acesso não Autorizado,<br>'.$_SESSION["403_message"].'.</p>';
+		}
+		unset($_SESSION["403_message"]);
 	}else{
-		$message='<p>Acesso não Autorizado,<br>'.$_SESSION["403_message"].'.</p>';
+		$message='<p>Acesso não Autorizado</p>';
 	}
-	unset($_SESSION["403_message"]);
-}else{
-	$message='<p>Acesso não Autorizado</p>';
+	return $message;	
 }
 $body_class="error";
 header('HTTP/1.1 403 Forbidden');
 include("inc/top.php");
-include("inc/nav_unregistered.php");
-echo '<h1 class=error><p class=error_type>403 Forbidden</p>'.$message.'</h1>';
+include("inc/nav_generic.php");
+echo '<h1 class=error><p class=error_type>403 Forbidden</p>'.get_message().'</h1>';
 include("inc/bottom.php");
 ?>
