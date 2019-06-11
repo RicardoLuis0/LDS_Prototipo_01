1adf164d8d96ff19e2ce8d598615fa0f486b5ab0
Ricardo <ricolvs123@gmail.com>
2019-06-10 21:45:56 -0300

    change autoload debug message

--- inc/autoload.php
+++ inc/autoload.php
@@ -3,10 +3,9 @@
 spl_autoload_register(function($class) {
 	$file='inc/'.str_replace('\\', '/', $class).'.php';
 	if(defined('AUTOLOAD_DEBUG')&&AUTOLOAD_DEBUG===true){
-		echo "<h2>Autoload</h2><p>Backtrace: </p><pre>";
+		echo "<h2>Autoload</h2><p>Loading: ".$file."</p><p>Backtrace: </p><pre>";
 		debug_print_backtrace();
 		echo "</pre>";
-		echo "<p>Trying to Load: ".$file."</p>";
 	}
 	include_once($file);
 });
